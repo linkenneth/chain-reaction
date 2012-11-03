daf9a816813f810efdb8cfcceaa4d20670ea251c
Kenneth Lin <kenlin@berkeley.edu>
2012-11-03 03:25:04 -0700

    oops

--- PlayMusic/bin/PlayMusic-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/link/code/chain-reaction/PlayMusic/bin/PlayMusic-debug-unaligned.apk : \
-/home/link/code/chain-reaction/PlayMusic/bin/PlayMusic.ap_ \
-/home/link/code/chain-reaction/PlayMusic/bin/classes.dex \
-/opt/android-sdk-linux/tools/support/annotations.jar \
-/home/link/code/chain-reaction/PlayMusic/libs/mail.jar \
