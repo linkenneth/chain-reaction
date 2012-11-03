d90b0410a314365e022f6bba5dac95ae5476a9a6
Kenneth Lin <kenlin@berkeley.edu>
2012-11-03 02:04:49 -0700

    FUCJ YEAH GOT THE ANDROID EMAIL APP TO WORK HOLLOOLLOLOLOL

--- PlayMusic/bin/PlayMusic-debug-unaligned.apk.d
+++ PlayMusic/bin/PlayMusic-debug-unaligned.apk.d
@@ -2,3 +2,4 @@
 /home/link/code/chain-reaction/PlayMusic/bin/PlayMusic.ap_ \
 /home/link/code/chain-reaction/PlayMusic/bin/classes.dex \
 /opt/android-sdk-linux/tools/support/annotations.jar \
+/home/link/code/chain-reaction/PlayMusic/libs/mail.jar \
