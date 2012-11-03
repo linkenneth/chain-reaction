e9534502a1dc9bdf077d615856f409a1d20faa5b
Kenneth Lin <kenlin@berkeley.edu>
2012-11-03 03:25:56 -0700

    Revert "oops"
    This reverts commit daf9a816813f810efdb8cfcceaa4d20670ea251c.

---
+++ PlayMusic/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/link/code/chain-reaction/PlayMusic/bin/AndroidManifest.xml : \
+/home/link/code/chain-reaction/PlayMusic/AndroidManifest.xml \
---
+++ PlayMusic/bin/PlayMusic-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+/home/link/code/chain-reaction/PlayMusic/bin/PlayMusic-debug-unaligned.apk : \
+/home/link/code/chain-reaction/PlayMusic/bin/PlayMusic.ap_ \
+/home/link/code/chain-reaction/PlayMusic/bin/classes.dex \
+/opt/android-sdk-linux/tools/support/annotations.jar \
+/home/link/code/chain-reaction/PlayMusic/libs/mail.jar \
